1bf59a469efd84e9677c9ad6e75efed60e27eb0d
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-11 00:13:03 +0900

    Feat: Header.tsx Create

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,71 @@
+// React
+import { Link } from "react-router-dom";
+// Style
+import { HeaderStyle } from "../style/HeaderStyle";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderStyle>
+      <div className="header">
+        <span className="title">
+          <Link to="/" id="title" className="link">
+            동물의 숲
+          </Link>
+        </span>
+
+        <span className="nav">
+          <Link className="link" to="/villagers">
+            마을주민
+          </Link>
+        </span>
+
+        <span className="nav">
+          <Link className="link" to="/">
+            동물
+          </Link>
+          <ul>
+            <Link to="/" className="nav-link">
+              물고기
+            </Link>
+            <Link to="/" className="nav-link">
+              곤충
+            </Link>
+            <Link to="/" className="nav-link">
+              바다생물
+            </Link>
+          </ul>
+        </span>
+
+        <span className="nav">
+          <Link className="link" to="/">
+            예술작품 및 화석
+          </Link>
+          <ul>
+            <Link to="/" className="nav-link">
+              예술작품
+            </Link>
+            <Link to="/" className="nav-link">
+              화석
+            </Link>
+          </ul>
+        </span>
+
+        <span className="nav">
+          <Link className="link" to="/">
+            음악
+          </Link>
+          <ul>
+            <Link to="/" className="nav-link">
+              노래
+            </Link>
+            <Link to="/" className="nav-link">
+              배경음악
+            </Link>
+          </ul>
+        </span>
+      </div>
+    </HeaderStyle>
+  );
 };
 
 export default Header;
