f8835f7e369a5106227a872e2f3088a175ded8a5
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-10 13:17:16 +0900

    Feat: Api 주소 추가

---
+++ src/api/ListApi.ts
@@ -0,0 +1,16 @@
+// 마을 주민
+export const Villagers = "https://bobbykjh.github.io/forest/villagers.json";
+
+// 음악
+export const Song = "https://bobbykjh.github.io/forest/songs.json";
+export const BackgroundMusic =
+  "https://bobbykjh.github.io/forest/backgroundmusic.json";
+
+// 생물
+export const Fish = "https://bobbykjh.github.io/forest/fish.json";
+export const Bug = "https://bobbykjh.github.io/forest/bugs.json";
+export const Sea = "https://bobbykjh.github.io/forest/sea.json";
+
+// 장식
+export const Art = "https://bobbykjh.github.io/forest/art.json";
+export const Fossil = "https://bobbykjh.github.io/forest/fossils.json";
