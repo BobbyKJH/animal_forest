d5085c27ad6b7b16774f9cde38834fcfba1467c1
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-12 11:23:21 +0900

    Feat: VillagerKind List Create

---
+++ src/api/VillagerKind.ts
@@ -0,0 +1,146 @@
+export const Kind = [
+  {
+    name: "모두",
+    value: "",
+  },
+  {
+    name: "개미햝기",
+    value: "Anteater",
+  },
+  {
+    name: "곰",
+    value: "Bear",
+  },
+  {
+    name: "새",
+    value: "Bird",
+  },
+  {
+    name: "황소",
+    value: "Bull",
+  },
+  {
+    name: "고양이",
+    value: "Cat",
+  },
+  {
+    name: "아기곰",
+    value: "Cub",
+  },
+  {
+    name: "닭",
+    value: "Chicken",
+  },
+  {
+    name: "소",
+    value: "Cow",
+  },
+  {
+    name: "악어",
+    value: "Alligator",
+  },
+  {
+    name: "사슴",
+    value: "Deer",
+  },
+  {
+    name: "개",
+    value: "Dog",
+  },
+  {
+    name: "오리",
+    value: "Duck",
+  },
+  {
+    name: "코끼리",
+    value: "Elephant",
+  },
+  {
+    name: "개구리",
+    value: "Frog",
+  },
+  {
+    name: "염소",
+    value: "Goat",
+  },
+  {
+    name: "고릴라",
+    value: "Gorilla",
+  },
+  {
+    name: "햄스터",
+    value: "Hamster",
+  },
+  {
+    name: "하마",
+    value: "Hippo",
+  },
+  {
+    name: "말",
+    value: "Horse",
+  },
+  {
+    name: "코알라",
+    value: "Koala",
+  },
+  {
+    name: "캥거루",
+    value: "Kangaroo",
+  },
+  {
+    name: "사자",
+    value: "Lion",
+  },
+  {
+    name: "원숭이",
+    value: "Monkey",
+  },
+  {
+    name: "쥐",
+    value: "Mouse",
+  },
+  {
+    name: "문어",
+    value: "Octopus",
+  },
+  {
+    name: "타조",
+    value: "Ostrich",
+  },
+  {
+    name: "독수리",
+    value: "Eagle",
+  },
+  {
+    name: "팽귄",
+    value: "Penguin",
+  },
+  {
+    name: "돼지",
+    value: "Pig",
+  },
+  {
+    name: "토끼",
+    value: "Rabbit",
+  },
+  {
+    name: "코뿔소",
+    value: "Rhino",
+  },
+  {
+    name: "양",
+    value: "Sheep",
+  },
+  {
+    name: "다람쥐",
+    value: "Squirrel",
+  },
+  {
+    name: "호랑이",
+    value: "Tiger",
+  },
+  {
+    name: "늑대",
+    value: "Wolf",
+  },
+];
