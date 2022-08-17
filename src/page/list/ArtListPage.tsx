415489af1d82d030b2be869067dbd8ab2d07c679
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-17 11:02:01 +0900

    Feat: ListPage Search Tech create

--- src/page/list/ArtListPage.tsx
+++ src/page/list/ArtListPage.tsx
@@ -1,14 +1,51 @@
+// React
+import { useCallback, useState } from "react";
 // Components
 import CardList from "../../components/list/CardList";
+import SearchList from "../../components/list/SearchList";
 // Style
 import { ListStyle } from "../../style/list/ListStyle";
 
 const ArtListPage = () => {
+  const [search, setSearch] = useState("");
+  const [btn, setBtn] = useState("");
+
+  const Search = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      setSearch(value);
+    },
+    [search]
+  );
+  const SearchBtn = useCallback(
+    (e: React.MouseEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setBtn(search);
+    },
+    [search]
+  );
+
   return (
     <ListStyle>
       <h2 className="list-title">예술 작품</h2>
 
-      <CardList thing="art" />
+      <form onSubmit={SearchBtn} className="search">
+        <input
+          type="text"
+          onChange={Search}
+          className="search-input"
+          placeholder="예술 작품을 입력해주세요."
+        />
+        <button type="submit" className="search-btn">
+          검색
+        </button>
+      </form>
+
+      {btn === "" ? (
+        <CardList thing="art" />
+      ) : (
+        <SearchList thing="art" search={btn} />
+      )}
     </ListStyle>
   );
 };
--- src/page/list/BugListPage.tsx
+++ src/page/list/BugListPage.tsx
@@ -1,14 +1,52 @@
+// React
+import { useCallback, useState } from "react";
 // Components
 import CardList from "../../components/list/CardList";
+import SearchList from "../../components/list/SearchList";
 // Style
 import { ListStyle } from "../../style/list/ListStyle";
 
 const BugListPage = () => {
+  const [search, setSearch] = useState("");
+  const [btn, setBtn] = useState("");
+
+  const Search = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      setSearch(value);
+    },
+    [search]
+  );
+
+  const SearchBtn = useCallback(
+    (e: React.MouseEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setBtn(search);
+    },
+    [search]
+  );
+
   return (
     <ListStyle>
       <h2 className="list-title">곤충 도감</h2>
 
-      <CardList thing="bugs" />
+      <form onSubmit={SearchBtn} className="search">
+        <input
+          type="text"
+          onChange={Search}
+          className="search-input"
+          placeholder="곤충 이름을 입력해주세요."
+        />
+        <button type="submit" className="search-btn">
+          검색
+        </button>
+      </form>
+
+      {btn === "" ? (
+        <CardList thing="bugs" />
+      ) : (
+        <SearchList thing="bugs" search={btn} />
+      )}
     </ListStyle>
   );
 };
