aa76946beedad477be0f64c1d5a152003c32de3a
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-18 00:19:56 +0900

    Refactor: villager Page && style

--- src/components/list/VillagersKind.tsx
+++ src/components/list/VillagersKind.tsx
@@ -1,27 +1,29 @@
 // React
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // Componenets
 import Card from "../common/Card";
+// Style
+import { CardStyle } from "../../style/common/CardStyle";
+import { SequenceBtn } from "../../style/common/ButtonStyle";
 // Api
-import { Villagers } from "../../api/ListApi";
+import instance from "../../api/instance";
 // Type
 import { Map } from "../../type/commonType";
-import { CardStyle } from "../../style/common/CardStyle";
 
 interface FilterCard {
   kind: string;
+  select: any;
 }
 
-const VillagersKind = ({ kind }: FilterCard) => {
+const VillagersKind = ({ kind, select }: FilterCard) => {
   const [list, setList] = useState([]);
+  const [page, setPage] = useState(0);
 
   const getList = async () => {
     try {
-      const res = await axios.get(Villagers);
+      const res = await instance.get("villagers.json");
       setList(res.data);
-      console.log(res.data);
     } catch (err) {
       alert("DB를 가져오는데 실패하였습니다.");
       console.error("에러: " + err);
@@ -30,22 +32,53 @@ const VillagersKind = ({ kind }: FilterCard) => {
 
   useEffect(() => {
     getList();
-  }, []);
+  }, [page]);
 
+  // 동물 종류 변경시 page 처음으로 돌려준다.
+  useEffect(() => {
+    setPage(0);
+  }, [select]);
+
+  // 동물 종류 filter
   const Kind = list.filter((res: { species: string }) =>
     res.species.includes(kind)
   );
 
+  const increase = () => {
+    if (page >= Kind.length - 9) {
+      return;
+    }
+    return setPage((prev) => prev + 9);
+  };
+
+  const decrease = () => {
+    if (page === 0) {
+      return;
+    }
+    setPage((prev) => prev - 9);
+  };
+
   return (
     <>
-      {Kind.map((item: Map) => (
+      {Kind.slice(page, page + 9).map((item: Map) => (
         <CardStyle key={item.id}>
-          <Link to={`/villager/${item.id}`}>
-            <Card icon={item.icon_uri} title={item.name_KRko} />
+          <Link to={`/villagers/${item.id}`}>
+            <Card image={item.icon_uri} title={item.name_KRko} />
           </Link>
         </CardStyle>
       ))}
-      {Kind.length}
+
+      <SequenceBtn>
+        <button className="btn" onClick={decrease}>
+          이전
+        </button>
+        <div className="list-number">
+          {page + 1} - {Kind.length}
+        </div>
+        <button className="btn" onClick={increase}>
+          다음
+        </button>
+      </SequenceBtn>
     </>
   );
 };
