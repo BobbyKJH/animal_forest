8aefc0ed6d83bbbfbad81aa6dee0c5001e536367
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-25 00:05:39 +0900

    Refactor: Redux State Change

--- src/components/list/SearchList.tsx
+++ src/components/list/SearchList.tsx
@@ -1,10 +1,14 @@
 // React
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+// Redux
+import { useAppDispatch } from "../../store/hooks";
 // Components
 import Card from "../common/Card";
+import NotValue from "../common/NotValue";
 // Style
 import { CardStyle } from "../../style/common/CardStyle";
+import { SequenceBtn } from "../../style/common/ButtonStyle";
 // Api
 import instance from "../../api/instance";
 // Type
@@ -13,10 +17,14 @@ import { Map } from "../../type/commonType";
 interface SearchType {
   thing: string;
   search: string;
+  page: number;
+  next: any;
+  previous: any;
 }
 
-const SearchList = ({ thing, search }: SearchType) => {
+const SearchList = ({ thing, search, page, next, previous }: SearchType) => {
   const [list, setList] = useState([]);
+  const conutPage = useAppDispatch();
 
   const getList = async () => {
     try {
@@ -27,23 +35,58 @@ const SearchList = ({ thing, search }: SearchType) => {
     }
   };
 
+  const ListFilter: any = list.filter((item: { name_KRko: string }) =>
+    item.name_KRko.includes(search)
+  );
+
+  const Next = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (page >= ListFilter.length - 9) {
+      return;
+    }
+    conutPage(next);
+  };
+
+  const Previous = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (page === 0) {
+      return;
+    }
+    conutPage(previous);
+  };
+
   useEffect(() => {
     getList();
   }, []);
 
-  const ListFilter = list.filter((item: { name_KRko: string }) =>
-    item.name_KRko.includes(search)
-  );
-
   return (
     <>
-      {ListFilter.map((item: Map) => (
+      {ListFilter.slice(page, page + 9).map((item: Map) => (
         <CardStyle key={item.id}>
-          <Link to={`/villagers/${item.id}`}>
-            <Card image={item.icon_uri} title={item.name_KRko} />
+          <Link to={`/${thing}/${item.id}`}>
+            {item.icon_uri ? (
+              <Card image={item.icon_uri} title={item.name_KRko} />
+            ) : (
+              <Card image={item.image_uri} title={item.name_KRko} />
+            )}
           </Link>
         </CardStyle>
       ))}
+
+      {/* 버튼 */}
+      {ListFilter.length === 0 ? (
+        <NotValue />
+      ) : (
+        <SequenceBtn>
+          <button className="btn" onClick={Previous}>
+            이전
+          </button>
+          <div className="list-number">
+            {page + 1} ~ {ListFilter.length}
+          </div>
+          <button className="btn" onClick={Next}>
+            다음
+          </button>
+        </SequenceBtn>
+      )}
     </>
   );
 };
