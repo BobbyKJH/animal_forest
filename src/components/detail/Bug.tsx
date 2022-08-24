2e54489e411ad9fe60ce5cda3eaf8b67e314cfab
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-24 16:06:49 +0900

    Refactor: Take && Museum add

--- src/components/detail/Bug.tsx
+++ src/components/detail/Bug.tsx
@@ -3,36 +3,38 @@ import { BugLocation, PriceCommas, Time } from "../../Utils";
 interface BugType {
   image: string;
   name: string;
+  take: string;
+  museum: string;
   location: string;
   time: string;
   price: number;
-  phrase: string;
-  museum: string;
 }
 
-const Bug = ({
-  image,
-  name,
-  phrase,
-  museum,
-  location,
-  time,
-  price,
-}: BugType) => {
+const Bug = ({ image, name, take, museum, location, time, price }: BugType) => {
   return (
     <>
       <img src={image} className="image" />
+
       <div className="intro">
         <span className="name">{name}</span>
-        <p className="phrase">{phrase}</p>
-        <p className="phrase">{museum}</p>
+
+        <div className="catch pick">
+          <p className="phrase">감정</p>
+          <p>{take}</p>
+        </div>
+
+        <div className="catch pick">
+          <p className="phrase">감정</p>
+          <p>{museum}</p>
+        </div>
 
         <div className="chart">
           <div className="chart-nav">
-            <p className="nav">출현 장소</p>
-            <p className="nav">출현 시간</p>
+            <p className="nav">출현장소</p>
+            <p className="nav">출현시간</p>
             <p className="nav">가격</p>
           </div>
+
           <div className="chart-nav">
             <p className="location nav">{BugLocation(location)}</p>
             <p className="time nav">{Time(time)}</p>
