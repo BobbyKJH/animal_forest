a1303a23ee08c9f36bc42b1ef30bbe4b57bbf96d
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-28 00:24:09 +0900

    Refactor: Main Logo image && Village, Creature, Work Banner

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,5 +1,24 @@
+// Componenets
+import CreatureBanner from "../components/main/CreatureBanner";
+import VillagersBanner from "../components/main/VillagersBanner";
+import WorkBanner from "../components/main/WorkBanner";
+// Style
+import { MainStyle } from "../style/main/MainStyle";
+// Image
+import LOGO from "../img/LOGO.png";
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <MainStyle>
+      <img src={LOGO} className="logo" />
+
+      <VillagersBanner />
+
+      <CreatureBanner />
+
+      <WorkBanner />
+    </MainStyle>
+  );
 };
 
 export default MainPage;
