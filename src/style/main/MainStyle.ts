23c6afe729c23ed184e9db73db79d992dcee79f5
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-31 19:30:42 +0900

    Style: MainPage Style Create

---
+++ src/style/main/MainStyle.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const MainStyle = styled.div`
+  .logo {
+    width: 1200px;
+    margin: 40px auto;
+    border: 5px solid #fff;
+    border-radius: 10px;
+  }
+  .banner {
+    display: block;
+    margin: 50px 0;
+  }
+`;
